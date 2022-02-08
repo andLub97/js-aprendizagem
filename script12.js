4f451329bbf3c9008be854155422fa025035ef60
André Lubawski <andrelubawski97@outlook.com>
2022-02-08 14:17:15 -0300

    coisas interessantes sobre manipulação de strings

--- script12.js
+++ script12.js
@@ -1,6 +1,20 @@
-var nome = "André";
-var idade =48;
-idade /= 2;
-console.log(nome, idade);
+//var nome = "André";
+//var idade =48;
+//idade /= 2;
+//console.log(nome, idade);
 
 const pi = 3.14;
+console.log(typeof pi);
+console.log(50 + '50');
+
+console.log("It's time");
+console.log("Entre \n\"Aspas\"");
+
+let nome = "André ";
+nome+="Lubawski";
+console.log(nome);
+
+const adj = "é f#da";
+let frase= "Programar em js ";
+frase+=adj;
+console.log(frase);
