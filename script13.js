ec3c006a8ad783fbc2b7fd7955450d5712b464f5
André Lubawski <andrelubawski97@outlook.com>
2022-02-09 13:42:54 -0300

    operadores lógicos

---
+++ script13.js
@@ -0,0 +1,7 @@
+var numero='80';
+var unidade='kg';
+var peso=numero+unidade;
+//como dividir o peso por dois e ter retorno numérico?
+var numero=+'80';
+var peso=numero/2+unidade;
+console.log(peso);
---
+++ script14.js
@@ -0,0 +1,16 @@
+/*var verdadeiro = 0;
+
+if (verdadeiro){
+    console.log("É verdade");
+}else if(!verdadeiro){
+    console.log('é falso');
+}
+else{
+    console.log('não sei');
+}*/
+var x = 10;
+var y = '10';
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);
+console.log(x!==y);
