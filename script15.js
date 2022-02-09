0fcfffa8c642c4008bc4ee7f6d4166f07270d785
André Lubawski <andrelubawski97@outlook.com>
2022-02-09 14:11:37 -0300

    switch

---
+++ script15.js
@@ -0,0 +1,12 @@
+var cor='azul';
+
+switch(cor){
+    case 'verde':
+        console.log('Cor azul');
+        break;
+    case 'azul':
+    console.log('azul');
+    break;
+    default:;
+    console.log('sem cor');
+}
