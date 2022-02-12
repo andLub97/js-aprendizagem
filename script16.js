415cefdd5b758267a6360189b4032ddd708cb396
André Lubawski <andrelubawski97@outlook.com>
2022-02-12 03:56:09 -0300

    funções

---
+++ script16.js
@@ -0,0 +1,15 @@
+/*function areaQuadrado(lado){
+    return lado * lado;
+}
+console.log(areaQuadrado(3));
+*/
+/*function imc(peso, altura){
+    var imc = peso/(altura*altura);
+    return imc;
+}
+console.log(imc(80, 1.8));
+*/
+
+addEventListener('click', function(){
+    console.log("oi");
+});
