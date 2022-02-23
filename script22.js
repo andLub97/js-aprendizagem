551378ff63ab1bcc2e14bff72c86fa42f167a68c
André Lubawski <andrelubawski97@outlook.com>
2022-02-23 14:59:50 -0300

    ternario

---
+++ script22.js
@@ -0,0 +1,5 @@
+var frutas = ['Banana','Pera','Maçã'];
+frutas.forEach(function(fruta, index){
+frutas.pop();
+console.log(fruta,index,frutas)
+});
---
+++ script23.js
@@ -0,0 +1,6 @@
+var idade = 20;
+
+var podeBeber;
+podeBeber = idade >= 18 ? true : false;
+
+console.log(podeBeber)
